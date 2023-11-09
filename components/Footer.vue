3e814359eeae03fd57a6efde5aae518b190f77ce
Ginasrrhmwti <141693381+Ginasrrhmwti@users.noreply.github.com>
2023-11-09 14:43:20 +0700

    Add files via upload

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <footer class="text-center text-light bg-secondary">
+      <p>Copyright 2023 by Hexail-tech</p>
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <carousel />
+
+    <about />
+
+    <FormPengunjung />
+
+    <Footer />
+
+  </div>
+</template>
+
+<script setup>
+useHead({ title: "Home - Aplikasi Buku Kunjungan" })
+</script>
